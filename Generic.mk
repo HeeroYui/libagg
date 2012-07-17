aa55ba51b9e12d93ec18ede6183d028904b4dc1d
Edouard DUPIN <yui.heero@gmail.com>
2012-07-17 14:03:26 +0200

    error in the tag display

--- Generic.mk
+++ Generic.mk
@@ -1,11 +1,12 @@
 
+# name of the librairy
+LOCAL_MODULE := agg
+
 # get the tag of the current project : 
 LOCAL_VERSION_TAG=$(shell cd $(LOCAL_PATH) ; git describe --tags)
 LOCAL_VERSION_TAG_SHORT=$(shell cd $(LOCAL_PATH) ; git describe --tags --abbrev=0)
 $(info $(LOCAL_MODULE) version TAG : $(LOCAL_VERSION_TAG))
 
-# name of the librairy
-LOCAL_MODULE := agg
 
 # name of the dependency
 LOCAL_STATIC_LIBRARIES := libfreetype
