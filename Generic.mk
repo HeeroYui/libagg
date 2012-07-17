65c901e41f5c8a79638bae2192ec418987ab9f70
Edouard DUPIN <yui.heero@gmail.com>
2012-07-17 13:41:21 +0200

    try to simplify makefiles

---
+++ Generic.mk
@@ -0,0 +1,31 @@
+
+# get the tag of the current project : 
+LOCAL_VERSION_TAG=$(shell cd $(LOCAL_PATH) ; git describe --tags)
+LOCAL_VERSION_TAG_SHORT=$(shell cd $(LOCAL_PATH) ; git describe --tags --abbrev=0)
+$(info $(LOCAL_MODULE) version TAG : $(LOCAL_VERSION_TAG))
+
+# name of the librairy
+LOCAL_MODULE := agg
+
+# name of the dependency
+LOCAL_STATIC_LIBRARIES := libfreetype
+
+LOCAL_C_INCLUDES := $(LOCAL_PATH) \
+                    $(LOCAL_PATH)/agg/ \
+                    $(LOCAL_PATH)/agg/util/
+
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)
+
+ifeq ($(DEBUG),1)
+LOCAL_CFLAGS := -DAGG_DEBUG_LEVEL=3 \
+                -DAGG_VERSION_TAG_NAME="\"$(LOCAL_VERSION_TAG_SHORT)-debug\""
+else
+LOCAL_CFLAGS := -DAGG_DEBUG_LEVEL=3 \
+                -DAGG_VERSION_TAG_NAME="\"$(LOCAL_VERSION_TAG_SHORT)-release\""
+endif
+
+# load the common sources file of the platform
+include $(LOCAL_PATH)/file.mk
+
+LOCAL_SRC_FILES := $(FILE_LIST)
+
