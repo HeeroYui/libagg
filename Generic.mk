197525dfc6cbe138767392466ad4f450aa7b2bd6
Edouard DUPIN <yui.heero@gmail.com>
2012-11-09 00:54:51 +0100

    [DEV] change tag position (no change with the time)

--- Generic.mk
+++ Generic.mk
@@ -4,12 +4,6 @@ include $(CLEAR_VARS)
 # name of the librairy
 LOCAL_MODULE := agg
 
-# get the tag of the current project : 
-LOCAL_VERSION_TAG=$(shell cd $(LOCAL_PATH) ; git describe --tags)
-LOCAL_VERSION_TAG_SHORT=$(shell cd $(LOCAL_PATH) ; git describe --tags --abbrev=0)
-$(info $(LOCAL_MODULE) version TAG : $(LOCAL_VERSION_TAG))
-
-
 # name of the dependency
 LOCAL_LIBRARIES := freetype etk
 
@@ -18,8 +12,6 @@ LOCAL_C_INCLUDES := $(LOCAL_PATH) \
                     $(LOCAL_PATH)/agg/util/
 
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)
-
-LOCAL_CFLAGS := -DAGG_VERSION_TAG_NAME="\"$(LOCAL_VERSION_TAG_SHORT)-$(BUILD_DIRECTORY_MODE)\""
 
 # load the common sources file of the platform
 include $(LOCAL_PATH)/file.mk
